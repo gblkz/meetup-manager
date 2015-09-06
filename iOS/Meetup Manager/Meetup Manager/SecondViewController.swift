698e9e2689b199f26eca9fcfb35b895f0f24274c
Teri Radichel <teri@radicalsoftware.com>
2015-09-05 19:45:21 -0700

    project structure

---
+++ iOS/Meetup Manager/Meetup Manager/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Meetup Manager
+//
+//  Created by Teri Radichel on 9/5/15.
+//  Copyright (c) 2015 Teri Radichel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
